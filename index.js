74936defff3ba00b05d70a89aba5a5dc2a8db2e5
govindbalki <govindbalki@yahoo.com>
2014-07-30 10:01:34 +0000

    v1

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express();
 
+var buffer = new Buffer(fs.readFile(index.html), "utf-8");
+var l_string = buffer.toString();
+
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Govind Balakrishnan!')
+  response.send(l_string)
 })
 
 app.listen(app.get('port'), function() {
