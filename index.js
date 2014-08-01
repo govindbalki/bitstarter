7aa677cac43e86074741c3a7e17317ce05f27261
govindbalki <govindbalki@yahoo.com>
2014-08-01 13:59:11 +0000

    V5

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express')
 var app = express();
 
 var l_text = fs.readFileSync('index.html','utf8');
-var buffer = new Buffer(fs.readFileSync('index.html','utf-8'),'utf-8');
+//var buffer = new Buffer(fs.readFileSync('index.html','utf-8'),'utf-8');
 
 //buffer=l_text;
-var l_string = buffer.toString('utf8',0,buffer.length);
+//var l_string = buffer.toString('utf8',0,buffer.length);
 //console.log(text);
 
 app.set('port', (process.env.PORT || 5000))
@@ -13,7 +13,6 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Just trying')
-  //response.send(buffer.toString())
 })
 
 app.listen(app.get('port'), function() {
