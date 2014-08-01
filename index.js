4293ad3c7cf0b6a99b4d4cd0767385808e8c32d9
govindbalki <govindbalki@yahoo.com>
2014-08-01 13:56:25 +0000

    V4

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(l_string)
+  response.send('Just trying')
   //response.send(buffer.toString())
 })
 
