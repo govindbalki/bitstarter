04abe9ca9319e72693c752fa56f8a5d35bc4339a
govindbalki <govindbalki@yahoo.com>
2014-08-01 15:23:55 +0000

    V8

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var buf = new Buffer('ABCDEF');
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
